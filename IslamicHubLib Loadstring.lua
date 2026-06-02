6bcb9db78c3ef834c61b1913661ba6ff9ddfb592
IslamicHubLib <nm9419506@gmail.com>
2026-06-02 19:14:03 -0400

    Update IslamicHubLib Loadstring.lua

--- IslamicHubLib Loadstring.lua
+++ IslamicHubLib Loadstring.lua
@@ -1,1 +1,1 @@
-local IslamicHub = loadstring(game:HttpGet("https://raw.githubusercontent.com/IslamicHubLib/Islam/refs/heads/main/IslamicHubLibOPENSOURCE", true))()
+local IslamicHub = loadstring(game:HttpGet("https://raw.githubusercontent.com/IslamicHubLib/Islam/refs/heads/main/IslamicHubLib%20OPEN%20SOURCE.lua", true))()
